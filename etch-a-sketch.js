2e5a045b9733dc713bd74636160d1327cd1b81a7
zeynepmimutlu <zeynepranamutlu@outlook.com>
2021-08-08 01:46:54 +0300

    Added eraser, hover mode and click mode

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,7 @@
 function makeGrids(){
     const gridContainer=document.querySelector('#grid-container')
     let gridSize=askGridSize()
-    if (gridSize=="") gridSize=10
+    if (gridSize=="" || gridSize==null) gridSize=10
     gridContainer.style['grid-template-columns']='repeat('+gridSize+',1fr)'
     let squareWidth=720/gridSize+'px'
     let squareHeight=720/gridSize+'px'
@@ -15,13 +15,61 @@ function makeGrids(){
             gridContainer.appendChild(div)
         }
     }
-    changeColorModetoBlack()
+    let playMode='hover'
+    changePlayMode(playMode)
+    const clickModeButton=document.querySelector('#click-mode')
+    const hoverModeButton=document.querySelector('#hover-mode')
+    clickModeButton.addEventListener('click',()=>{
+      playMode='click'
+      changePlayMode(playMode)
+    })
+    hoverModeButton.addEventListener('click',()=>{
+      playMode='hover'
+      changePlayMode(playMode)
+    })
+    const cleanButton = document.querySelector('#clean-button')
+    cleanButton.addEventListener('click', cleanGrids)
+
+    const rainbowButton=document.querySelector('#rainbow-button')
+    rainbowButton.addEventListener('click',()=>{colorMode='rainbow'})
+
+    const bwButton=document.querySelector('#bw-button')
+    bwButton.addEventListener('click',()=>{colorMode='b&w'})
+
+    const eraserButton=document.querySelector('#eraser-button')
+    eraserButton.addEventListener('click',()=>{colorMode='white'})
+
+    const changeSizeButton=document.querySelector('#change-size-button')
+    changeSizeButton.addEventListener('click',()=>{
+        while (gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.lastChild)
+      }
+      makeGrids()
+    })
 }
-function changeColorModetoRainbow(){
-    colorMode='rainbow'
-}
-function changeColorModetoBlack(){
-    colorMode='b&w'
+function changePlayMode(playMode){
+  const squares = document.getElementsByClassName('squares')
+  if (playMode=='click'){
+  for(let i=0;i<squares.length;i++){
+    squares[i].removeEventListener('mouseover',changeColor)
+    squares[i].addEventListener('mousedown',()=>{
+      for(let k=0;k<squares.length;k++){
+        squares[k].addEventListener('mouseover',changeColor)
+      }
+    })   
+    squares[i].addEventListener('mouseup',()=>{
+      for(let k=0;k<squares.length;k++){
+        squares[k].removeEventListener('mouseover',changeColor)
+      }
+    })
+  }
+  }
+  else{
+  for(let i=0;i<squares.length;i++){
+    squares[i].removeEventListener('mousedown',changeColor)
+    squares[i].addEventListener('mouseover',changeColor)
+  }
+  }
 }
 function askGridSize(){
     gridSize=prompt('Please enter a grid size between 1 and 100.')
@@ -37,7 +85,7 @@ function askGridSize(){
 }
 function cleanGrids(){
     const squares = document.getElementsByClassName('squares')
-    for(let i=0;i<square.length;i++){
+    for(let i=0;i<squares.length;i++){
         squares[i].style['background-color']='white'
     }
 }
@@ -45,8 +93,11 @@ function changeColor(){
     if (colorMode=='rainbow'){
         this.style['background-color']=getRandomColor()
     }
-    else if (colorMode='b&w'){
+    else if (colorMode=='b&w'){
         this.style['background-color']='black'
+    }
+    else if (colorMode=='white'){
+      this.style['background-color']='white'
     }
 }
 function getRandomColor(){
@@ -57,26 +108,5 @@ function getRandomColor(){
     }
     return randomColor
 }
+let colorMode='b&w'
 makeGrids()
-const cleanButton = document.querySelector('#clean-button')
-const rainbowButton=document.querySelector('#rainbow-button')
-const bwButton=document.querySelector('#bw-button')
-const changeSizeButton=document.querySelector('#change-size-button')
-
-cleanButton.addEventListener('click', cleanGrids)
-rainbowButton.addEventListener('click',changeColorModetoRainbow)
-bwButton.addEventListener('click',changeColorModetoBlack)
-changeSizeButton.addEventListener('click',()=>{
-  const gridContainer=document.querySelector('#grid-container')
-  const squares = document.getElementsByClassName('squares')
-  while (gridContainer.firstChild){
-    gridContainer.removeChild(gridContainer.lastChild)
-  }
-  makeGrids()
-  for(let i=0;i<squares.length;i++){
-    squares[i].addEventListener('mouseover',changeColor)
-  }
-})
-for(let i=0;i<square.length;i++){
-  squares[i].addEventListener('mouseover',changeColor)
-}
